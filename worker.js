43a9b7ef4015e98a29c34acd3c1a4c339887d44f
PolosatiyKot <120979953+PolosatiyKot@users.noreply.github.com>
2026-09-25 09:41:00 +0000

    Update worker.js

--- worker.js
+++ worker.js
@@ -8,13 +8,6 @@ export default {
 
 const url = new URL(request.url);
 
-if (url.pathname === "/api/debug-secrets" && request.method === "GET") {
-  return json({
-    admin: Boolean(env.INITIAL_ADMIN_PASSWORD),
-    user: Boolean(env.INITIAL_USER_PASSWORD)
-  });
-}
-
 if (url.pathname === "/api/login" && request.method === "POST") {
         return await login(request, env);
       }
